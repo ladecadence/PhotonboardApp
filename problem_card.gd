4ebc0a0614d930c4209b0255bd0002b07e040b76
David Pello <dvpg@tuta.io>
2025-03-13 14:41:13 +0100

    Renamed objects for consistency

--- problem_card.gd
+++ problem_card.gd
@@ -12,13 +12,13 @@ func _process(delta: float) -> void:
 	pass
 
 func load_data(data: Problem):
-	$Fondo/HBoxContainer/Descr/Nombre.text = data.name
-	$Fondo/HBoxContainer/Descr/Resumen.text = data.description
+	$Fondo/HBoxContainer/Descr/Name.text = data.name
+	$Fondo/HBoxContainer/Descr/Description.text = data.description
 	# calculate stars
 	var stars = ""
 	for i in data.rating:
 		stars += "⭐"
-	$Fondo/HBoxContainer/Descr/Estrellas.text = stars
-	$Fondo/HBoxContainer/Datos/MarginContainer/CenterContainer/Panel/Grado.text = data.grade
-	$Fondo/HBoxContainer/Datos/Sends/Numero.text = str(data.sends)
+	$Fondo/HBoxContainer/Descr/Stars.text = stars
+	$Fondo/HBoxContainer/Data/MarginContainer/CenterContainer/Panel/Grade.text = data.grade
+	$Fondo/HBoxContainer/Data/Sends/Number.text = str(data.sends)
 	
