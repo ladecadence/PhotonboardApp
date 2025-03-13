58ea56696745349babc903d1a7d13eb3749dc929
David Pello <dvpg@tuta.io>
2025-03-13 14:41:37 +0100

    Test data load

--- content_problem.gd
+++ content_problem.gd
@@ -2,3 +2,11 @@ extends Control
 
 const Hold = preload("res://scripts/Hold.gd")
 const Problem = preload("res://scripts/Problem.gd")
+
+func _ready() -> void:
+	pass
+	
+func loadData(data: Problem):
+	$Scroll/Lista/MarginContainer/Image.texture = ImageTexture.create_from_image(Image.load_from_file("res://images/wall0001.jpg"))
+	$Scroll/Lista/Name.text = data.name
+	$Scroll/Lista/Description.text = data.description
