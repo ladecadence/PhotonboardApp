28b4a7dfd19935c60e5e9eff9b566aa2501f48df
David Pello <zako@ladecadence.net>
2025-03-16 21:44:10 +0100

    Added offset for adjust widget position in screen

--- components/wall_new_2.gd
+++ components/wall_new_2.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+func _ready() -> void:
+	$VBoxContainer/MarginContainer/WallWidget.setOffset($VBoxContainer/Header.get_rect().size)
 
 func loadData(data):
 	$VBoxContainer/MarginContainer/WallWidget.loadData(data)
