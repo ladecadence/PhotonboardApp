19dd65f810c4829cbef4c2db78d47de78875d9c2
David Pello <zako@ladecadence.net>
2025-03-16 21:42:58 +0100

    Changed hold colors

--- scripts/Hold.gd
+++ scripts/Hold.gd
@@ -5,7 +5,7 @@ class_name Hold
 enum HOLD_TYPE {DESIGN, START, FOOT, HOLD, TOP}
 enum HOLD_SIZE {SMALL = 20, MEDIUM = 30, BIG = 40}
 
-const holdColors = [Color(Color.DIM_GRAY, 0.5) , Color(Color.AQUAMARINE, 0.8), Color(Color.CORAL, 0.8), Color(Color.DEEP_SKY_BLUE, 0.8), Color(Color.VIOLET, 0.8)]
+const holdColors = [Color(Color.CRIMSON, 0.5) , Color(Color.AQUAMARINE, 0.8), Color(Color.CORAL, 0.8), Color(Color.DEEP_SKY_BLUE, 0.8), Color(Color.VIOLET, 0.8)]
 
 var id: int
 var wallid: int
