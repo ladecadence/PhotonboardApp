767387b67d77c69f52216233ee9c96a354fcf14e
David Pello <dvpg@tuta.io>
2025-03-26 15:24:43 +0100

    Connect signals for correct navigation

--- components/content_wall_new.gd
+++ components/content_wall_new.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 func _on_panel_cancel_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		# Back to wall list
-		AppManager.load_screen(AppManager.Screen.PROBLEM_LIST, null)
+		AppManager.load_screen(AppManager.Screen.WALL_LIST, null)
 
 
 func _on_panel_continue_gui_input(event: InputEvent) -> void:
--- scripts/content_problem_new.gd
+++ scripts/content_problem_new.gd
@@ -5,3 +5,14 @@ func _ready() -> void:
 		$MarginContainer/Scroll/Lista/HBoxContainer6/OptionWall.add_item(wall.name)
 	for grade in Grade.GRADES_FONT:
 		$MarginContainer/Scroll/Lista/HBoxContainer4/OptionGrade.add_item(Grade.GRADES_FONT[grade])
+
+
+
+func _on_panel_continue_gui_input(event: InputEvent) -> void:
+	pass # Replace with function body.
+
+
+func _on_panel_cancel_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		# Back to wall list
+		AppManager.load_screen(AppManager.Screen.PROBLEM_LIST, null)
