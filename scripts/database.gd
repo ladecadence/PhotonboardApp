dc84b1091b5379bac6793365fb34f64929cde530
David Pello <dvpg@tuta.io>
2025-03-27 16:04:53 +0100

    Update data in DB if data already exists

--- scripts/database.gd
+++ scripts/database.gd
@@ -167,7 +167,7 @@ func get_db_problem(id: String) -> Problem:
 	# Open the database using the db_name found in the path variable
 	db.open_db()
 	
-	# get wall from id
+	# get problem from id
 	var _selected_array = db.select_rows("problems", 'id="'+id+'"', ["*"])
 	var query_result : Array = db.query_result
 	# Close the current database
@@ -217,8 +217,15 @@ func insert_db_problem(p: Problem):
 	# Open the database using the db_name found in the path variable
 	db.open_db()
 	
-	# insert it
-	db.insert_row("problems", p.to_dict())
+	# check if it exists
+	var _selected_array = db.select_rows("problems", 'id="'+p.id+'"', ["*"])
+	var query_result : Array = db.query_result
+	if query_result.is_empty():
+		# if it's empty insert it
+		db.insert_row("problems", p.to_dict())
+	else:
+		# update it
+		db.update_rows("problems", 'id="'+p.id+'"', p.to_dict())
 	
 	# Close the current database
 	db.close_db()
@@ -230,8 +237,16 @@ func insert_db_wall(w: Wall):
 	# Open the database using the db_name found in the path variable
 	db.open_db()
 	
-	# insert it
-	db.insert_row("walls", w.to_dict())
+	# check if it exists
+	# get wall from id
+	var _selected_array = db.select_rows("walls", 'id="'+w.id+'"', ["*"])
+	var query_result : Array = db.query_result
+	if query_result.is_empty():
+		# if it's empty insert it
+		db.insert_row("walls", w.to_dict())
+	else:
+		# update it
+		db.update_rows("walls", 'id="'+w.id+'"', w.to_dict())
 	
 	# Close the current database
 	db.close_db()
