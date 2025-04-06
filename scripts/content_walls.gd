57615b5d40e0aca0359ca58e0e08698b28f9f93c
eneko-osia <eneko@tuta.com>
2025-04-06 10:47:38 +0200

    initial wall async loading

--- scripts/content_walls.gd
+++ scripts/content_walls.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var lista = $Scroll/Lista
 @onready var card = preload("res://components/wall_card.tscn")
 
+var load_thread := Thread.new()
+
 #const Wall = preload("res://scripts/Wall.gd")
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +13,9 @@ func _ready() -> void:
 	# delete cards
 	for child in lista.get_children():
 		child.queue_free()
-	
-	# load data
-	var walls = Database.get_db_walls()
-	for w in walls:
-		var c = card.instantiate()
-		c.load_data(w)
-		lista.add_child(c)
+
+	# load walls
+	load_thread.start(Callable(self, "_load_walls_async"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -35,3 +33,13 @@ func _on_panel_add_gui_input(event: InputEvent) -> void:
 
 func _on_button_config_pressed() -> void:
 	AppManager.load_screen(AppManager.Screen.CONFIG, null)
+	
+func _load_walls_async():
+	call_deferred("_on_walls_loaded", Database.get_db_walls())
+	
+func _on_walls_loaded(walls):
+	for w in walls:
+		var c = card.instantiate()
+		c.load_data(w)
+		lista.add_child(c)
+	load_thread.wait_to_finish()
