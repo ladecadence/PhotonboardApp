4a278057e86364bb6ed7dbae658ed1d623429ad2
eneko-osia <eneko@tuta.com>
2025-04-08 15:58:44 +0200

    thread_pool file renamed to ThreadPool since it is a class

--- scripts/thread/ThreadPool.gd
+++ scripts/thread/ThreadPool.gd
@@ -1,6 +1,8 @@
 extends Object
 class_name ThreadPool
 
+# attributes
+
 const MIN_THREADS: int = 1
 var _queue: Array = []
 var _queue_mutex: Mutex = Mutex.new()
@@ -8,8 +10,10 @@ var _running: bool = true
 var _running_mutex: Mutex = Mutex.new()
 var _threads: Array[Thread] = []
 
+# public methods
+
 func destroy() -> void:
-	print("[Thread: %2s] ThreadPool.destroy ({ \"threads\": %d })" % [OS.get_thread_caller_id(), _threads.size()])
+	print("[Thread: %2s] ThreadPool.destroy { \"threads\": %d }" % [OS.get_thread_caller_id(), _threads.size()])
 	_running_mutex.lock()
 	_running = false
 	_running_mutex.unlock()
@@ -20,13 +24,15 @@ func destroy() -> void:
 
 func request(task: Callable, args: Array = [], callback: Callable = Callable()) -> void:
 	var work = { "task": task, "args": args, "callback": callback }
-	print("[Thread: %2s] ThreadPool.request (%s)" % [OS.get_thread_caller_id(), work])
+	print("[Thread: %2s] ThreadPool.request { \"task\": %s, \"callback:\" %s}" % [OS.get_thread_caller_id(), work.task, work.callback])
 	_queue_mutex.lock()
 	_queue.append(work)
 	_queue_mutex.unlock()
 
+# private methods
+
 func _init(threads: int) -> void:
-	print("[Thread: %2s] ThreadPool._init ({ \"threads\": %d })" % [OS.get_thread_caller_id(), threads])
+	print("[Thread: %2s] ThreadPool._init { \"threads\": %d }" % [OS.get_thread_caller_id(), threads])
 	for i in range(max(MIN_THREADS, threads)):
 		var thread = Thread.new()
 		if (thread.start(_process_queue) == OK):
@@ -45,16 +51,16 @@ func _process_queue() -> void:
 		var work = _queue.pop_front()
 		_queue_mutex.unlock()
 		if (work != null):
-			print("[Thread: %2s] ThreadPool._process_queue (\"size:\" %s)" % [OS.get_thread_caller_id(), queue_size])
+			print("[Thread: %2s] ThreadPool._process_queue { \"task\": %s, \"callback:\" %s} {\"queue_size:\" %s}" % [OS.get_thread_caller_id(), work.task, work.callback, queue_size])
 			_process_work(work)
 		OS.delay_msec(1)
 
 func _process_work(work: Dictionary) -> void:
-	print("[Thread: %2s] ThreadPool._process_work (%s)" % [OS.get_thread_caller_id(), work])
+	print("[Thread: %2s] ThreadPool._process_work { \"task\": %s, \"callback:\" %s}" % [OS.get_thread_caller_id(), work.task, work.callback])
 	var results = work.task.callv(work.args)
-	call_deferred("_work_done", work.callback, results)
+	call_deferred("_work_done", work, results)
 
-func _work_done(callback: Callable, results) -> void:
-	print("[Thread: %2s] ThreadPool._work_done ({ \"callback\": %s })" % [OS.get_thread_caller_id(), callback])
-	if callback.is_valid():
-		callback.call(results)
+func _work_done(work: Dictionary, results) -> void:
+	print("[Thread: %2s] ThreadPool._work_done { \"task\": %s, \"callback:\" %s}" % [OS.get_thread_caller_id(), work.task, work.callback])
+	if work.callback.is_valid():
+		work.callback.callv([results])
