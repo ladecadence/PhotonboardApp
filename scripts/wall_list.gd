a6fab449fdc5aa302da444b09cb49db2d7fdee55
David Pello <dvpg@tuta.io>
2025-04-16 12:01:11 +0200

    Use header scene

--- scripts/wall_list.gd
+++ scripts/wall_list.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+func _ready() -> void:
+	$VBoxContainer/Header2.set_title("Walls")
 
 func _on_panel_add_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
