a19ccd77fb5c0e8dacc00734fbc97cd5c9161681
eneko-osia <eneko@tuta.com>
2025-04-22 10:38:25 +0200

    add functionality to change data source from the config screen

--- scripts/config.gd
+++ scripts/config.gd
@@ -4,6 +4,9 @@ extends MarginContainer
 @onready var lineEditIP = $VBoxContainer/Control/MarginContainer/Scroll/Lista/HBoxContainer/LineEditIP
 @onready var fontRadio = $VBoxContainer/Control/MarginContainer/Scroll/Lista/HBoxContainer2/CheckBoxFont
 @onready var huecoRadio = $VBoxContainer/Control/MarginContainer/Scroll/Lista/HBoxContainer2/CheckBoxHueco
+@onready var dataSourceOptionButton = $VBoxContainer/Control/MarginContainer/Scroll/Lista/HBoxContainerDataSource/DataSourceOptionButton
+
+var _data_source: AppManager.DataSource = AppManager.DataSource.LOCAL
 
 func _ready() -> void:
 	$VBoxContainer/Header2.set_title("Config")
@@ -12,11 +15,12 @@ func _ready() -> void:
 		fontRadio.button_pressed = true
 	else:
 		huecoRadio.button_pressed = true
-
+	dataSourceOptionButton.selected = AppManager.data_source
 
 func _on_panel_save_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		AppManager.wall_ip = lineEditIP.text
+		AppManager.data_source = dataSourceOptionButton.selected
 		AppManager.save_config()
 		# show and hide config saved text
 		labelOk.text = "Config Saved"
